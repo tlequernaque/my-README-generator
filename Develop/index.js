b662bf8417749dcca0ed8b4e504548c6f877bb3c
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-02-25 00:04:55 -0500

    added packages and wrote questions for readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    '1: What is your GitzHub username?',
+    '2: What is your email address?',
+    '3: What is your projects name?',
+    '4: Please write a short description of your project?',
+    '5: What kind of license should your project have?',
+    '6: What command should be run to install dependencies?',
+    '7: What command should be run to run tests?',
+    '8: What does the user need to know about using your repo?',
+    '9: What does the user need to know about contributing to the repo?',
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
